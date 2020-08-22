3557cb132ab234866cb0d6fab85a767b20b1a2d7
Zenahr Barzani <zenmatica@gmail.com>
2020-08-22 02:52:58 +0200

    changed model

--- src/_app.py
+++ src/_app.py
@@ -6,45 +6,45 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = 'secret'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/anime'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/Users'
 
 mongo = PyMongo(app)
-conn = mongo.db.anime
 
-@app.route('/api/v1/anime/add', methods=['POST'])
-def add_item():
+@app.route('/api/v1/add', methods=['POST'])
+def add_user():
     json = request.json
-    anime = json['anime'] 
-    season = json['season'] 
-    episode = json['episode']
-    rating = json['rating']
+    name = json['name'] 
+    email = json['email'] 
+    password = json['password'] 
 
-    if request.method ==  'POST' and anime and season and episode and rating:
-
+    if name and email and password and request.method ==  'POST':
+        password_hash = generate_password_hash(password)
+        
         payload = {
-            'anime': anime, 
-            'season': season, 
-            'episode': episode,
-            'rating': rating,
+            'name':name,
+            'email':email,
+            'password':password_hash,
         }
-
-        conn.insert_one(payload)
+        
+        mongo.db.users.insert_one(payload)
+        del payload['_id']
+        del payload['password']
         response = dumps(payload)
         return Response(response, status=200, mimetype='application/json')
 
-@app.route('/api/v1/anime', methods=['GET'])
-def items():
-    return Response(dumps(conn.find()), mimetype='application/json')
+@app.route('/api/v1/users', methods=['GET'])
+def users():
+    return Response(dumps(mongo.db.users.find()), mimetype='application/json')
 
-@app.route('/api/v1/anime/<anime_id>', methods=['GET', 'DELETE'])
-def item(item_id):
+@app.route('/api/v1/users/<user_id>', methods=['GET', 'DELETE'])
+def user(user_id):
     if request.method ==  'GET':
-        response = dumps(conn.find_one({'_id': ObjectId(item_id)}))
+        response = dumps(mongo.db.users.find_one({'_id': ObjectId(user_id)}))
         print(response)
         return Response(response, status=200, mimetype='application/json')
     if request.method ==  'DELETE': # TODO: check if user exists and tell in response, otherwise delete.
-        conn.delete_one({'_id': ObjectId(item_id)})
-        return Response("Entry deleted.", status=200, mimetype='text/plain')
+        mongo.db.users.delete_one({'_id': ObjectId(user_id)})
+        return Response("User deleted.", status=200, mimetype='text/plain')
 
 
 @app.errorhandler(404)
